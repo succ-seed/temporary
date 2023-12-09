aa9196007b09d2c8788f032a87a0dcf36fa664a3
succ-seed <sandeshtiwari0211@gmail.com>
2023-12-09 12:33:19 +0545

    first commmit and comment from git bash

---
+++ 05_memory_allocation.cpp
@@ -0,0 +1,22 @@
+//!  when we make a class, there's variables and there are method or function
+//! lets take a example of variable being glass and method being a container of juice
+//! for every object or person, we have different glass but the container that we pour from is the same
+
+//! similiarly in c++, for every different object, individual variable is made, but the method/function is made once and shared
+
+#include <iostream>
+
+using namespace std;
+
+class shop
+{
+    int itemid[100];
+    int itemprice[100];
+    int counter;
+
+public:
+    void initcounter(void)
+    {
+        counter++;
+    }
+};
